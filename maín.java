adfabacd599743eb19c8ba7f531367b264be7439
Christopher Rodger <christopher.rodger@gmail.com>
2014-09-01 10:09:36 +0100

    Update maín.java

--- maín.java
+++ maín.java
@@ -1,1 +1,9 @@
-//this is a test
+/**
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+}
